ce51b87e880f435424fe9dbee0c85a48a7127f0a
kauandragues <kaua.rodrigues1509@gmail.com>
2026-03-03 23:35:40 -0300

    aulas do dia 03/03/2026

---
+++ curso_luiz_otavio/aulas_e_execicios/aula74.5/teste_x.py
@@ -0,0 +1,5 @@
+import x 
+
+namespace_x = globals()
+print(namespace_x)
+x.func_global()
---
+++ curso_luiz_otavio/aulas_e_execicios/aula74.5/x.py
@@ -0,0 +1,4 @@
+def func_global() -> None:
+    
+    print("Função func_global() do x.py")
+    print(globals())
