d537c354e044ff8eccabe7e6b4e7a0c1f0bd57a2
kauandragues <kaua.rodrigues1509@gmail.com>
2026-03-05 18:40:48 -0300

    aulas dia 05/03/2026

--- om/aulas_e_execicios/aula74.5/teste_x.py
+++ om/aulas_e_execicios/aula74.5/teste_x.py
@@ -1,5 +1,14 @@
-import x 
+nome_global = "nome_global"
 
-namespace_x = globals()
-print(namespace_x)
-x.func_global()
+def func_global() -> None:
+    nome_enclosing = "Foda" # Enclosing (Local)
+
+    def func_interna() -> None:
+        nonlocal nome_enclosing
+        nome_enclosing = "MUDAR"
+        print(nome_enclosing, id(nome_enclosing))
+
+    print(nome_enclosing, id(nome_enclosing))
+    func_interna()
+
+func_global()
